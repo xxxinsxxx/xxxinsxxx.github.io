b5215391a57f3ae39ab521f224df5e11e3feec97
shiyi <529977910@qq.com>
2023-03-01 15:55:34 +0800

    :white_check_mark: 添加测试代码

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  base: './',
+  resolve: {
+    alias: {
+      // 配置别名
+      "@": resolve(__dirname, "./src"),
+    }
+  },
+  plugins: [
+      vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ]
 })
